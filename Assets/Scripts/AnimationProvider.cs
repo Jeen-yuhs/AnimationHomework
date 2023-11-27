ef1f8bf1e7eb8eede26b793fe48861977305eaec
Jeen-yuhs <132693271+Jeen-yuhs@users.noreply.github.com>
2023-11-27 16:14:08 +0600

    Jump animation added

--- Assets/Scripts/AnimationProvider.cs
+++ Assets/Scripts/AnimationProvider.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +6,21 @@ public class AnimationProvider : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private Button _danceButton;
     [SerializeField] private Slider _movementSlider;
+    [SerializeField] private Button _jumpButton;
 
     private void Start()
     {
+        _jumpButton.onClick.AddListener(Jump); 
         _danceButton.onClick.AddListener(Dance);
         _movementSlider.onValueChanged.AddListener(MoveCharacter);
+    }
+
+    private void OnDestroy()
+    {
+        _danceButton.onClick.RemoveListener(Dance);
+        _jumpButton.onClick.RemoveListener(Jump);
+
+        _movementSlider.onValueChanged.RemoveListener(MoveCharacter);
     }
 
     private void MoveCharacter(float sliderValue)
@@ -19,14 +28,13 @@ public class AnimationProvider : MonoBehaviour
         _animator.SetFloat("MovementSpeed", sliderValue);
     }
 
-    private void OnDestroy()
-    {
-        _danceButton.onClick.RemoveListener(Dance);
-        _movementSlider.onValueChanged.RemoveListener(MoveCharacter);
-    }
-
     private void Dance()
     {
         _animator.SetTrigger("Dance");
+    }
+
+    private void Jump()
+    {
+        _animator.SetTrigger("Jump");
     }
 }
